db1a7147dd856ce59119488f3cf98a2e6db5655f
Juan Ramon Fernandez M <2024-0070@unicda.edu.do>
2026-06-08 23:04:22 -0400

    Refactor: remove redundant Estudiante class from SistemaCalificaciones and create a separate Estudiante.java file

--- SistemaCalificaciones.java
+++ SistemaCalificaciones.java
@@ -197,25 +197,3 @@ public class SistemaCalificaciones {
     }
 }
 
-class Estudiante {
-
-    String mes;
-    String curso;
-    String nombre;
-    String apellido;
-
-    int matematica;
-    int lengua;
-    int naturales;
-    int sociales;
-
-    public double calcularPromedio() {
-        int cantidadMaterias = 4;
-
-        try {
-            return (matematica + lengua + naturales + sociales) / cantidadMaterias;
-        } catch (ArithmeticException e) {
-            return 0;
-        }
-    }
-}
